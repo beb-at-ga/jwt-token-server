9233324dd055979007cb21af2b8eec96bf60b7b5
Bryan Brinson <bryan@beb.com>
2019-09-12 11:48:53 -0700

    completed /login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,43 @@
-require('dotenv').config();
-const router = require('express').Router();
-const db = require('../models');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const router = require("express").Router();
+const db = require("../models");
+const jwt = require("jsonwebtoken");
 
-router.post('/login', (req, res) => {
-  res.send(req.body);
+// POST /auth/login (find and validate user and send password)
+router.post("/login", (req, res) => {
+  // Find the user by their email address
+  db.User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      if (!user || !user.password) {
+        return res.status(406).send({
+          message: "Invalid Credentials"
+        });
+      }
+
+      if (!user.isAuthenticated(req.body.password)) {
+        // password did not match
+        return res.status(406).send({
+          message: "Invalid credentials"
+        });
+      }
+
+      let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+        expiresIn: 60 * 60
+      });
+
+      res.send({ token });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(503).send({
+        message: "Something is amiss."
+      });
+    });
 });
 
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   // res.send(req.body);
   db.User.findOne({
     email: req.body.email
@@ -16,7 +46,7 @@ router.post('/signup', (req, res) => {
       // if user exists, do not let them create a duplicate account
       if (user) {
         return res.status(409).send({
-          message: 'Email address already in use.'
+          message: "Email address already in use."
         });
       } else {
         db.User.create(req.body)
@@ -33,7 +63,7 @@ router.post('/signup', (req, res) => {
           .catch(err => {
             console.log(err);
             res.status(500).send({
-              message: 'Error creating new customer record.'
+              message: "Error creating new customer record."
             });
           });
       }
@@ -41,13 +71,22 @@ router.post('/signup', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(503).send({
-        message: 'Error accessing database. Please try again.'
+        message: "Error accessing database. Please try again."
       });
     });
 });
 
-router.get('/current/user', (req, res) => {
-  res.send('on /current/user');
+router.get("/current/user", (req, res) => {
+  if (!req.user) {
+    res.status(500).send({ message: 'Something went wrong. Please try again.'})
+  } else {
+    // This is the user data from the time the token was issued, therfore if the user is updated,
+    // those values will not be reflected here...
+
+    // to avoid this problem, reissue token when you update their data. 
+    // login, update, 
+    res.json(req.user);
+  }
 });
 
 module.exports = router;
